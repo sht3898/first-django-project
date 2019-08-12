406cff94d011bc90ec8bc0a939c62c7f7becf674
sht3898 <sehyunteg@naver.com>
2019-08-12 14:43:21 +0900

    05_variable routing

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,3 +10,20 @@ def index(request):
     # 3. 해당하는 템플릿 반환
     return render(request, 'index.html')
 
+def hello(request, name):
+    context = {'name' : name}
+    return render(request, 'hello.html', context)
+
+def lotto(request): # request는 임의로 지은 변수라 다른 이름으로 수정해도 됨
+    print(request)
+    print(type(request))
+    print(request.META)
+    # 로직
+    numbers = sorted(random.sample(range(1, 46), 6))
+    # 값을 딕셔너리에 담아서(보통 context라고 부름) 보낸다.
+    context = {'numbers': numbers}
+    # render 할때 3번째 인자로 넘겨준다.
+    # render 함수의 필수 인자 : request, template 파일
+    # 변수를 넘겨주고 싶으면 3번째 인자로 dictionary를 넘겨준다.
+    # Django에서 활용하는 템플릿 언어는 Django Template Language(DTL)! Jinja와 비슷하지만 다른 언어
+    return render(request, 'lotto.html', context)
